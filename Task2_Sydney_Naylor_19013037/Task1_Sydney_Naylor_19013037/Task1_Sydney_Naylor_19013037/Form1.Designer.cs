3f7907864f260584734b6c672418e5dc1fc289a1
coconutZzx <naylorsydney@gmail.com>
2019-10-07 14:01:41 +0200

    Form1.Designer.cs

---
+++ Task2_Sydney_Naylor_19013037/Task1_Sydney_Naylor_19013037/Task1_Sydney_Naylor_19013037/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Task1_Sydney_Naylor_19013037
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRounds = new System.Windows.Forms.Label();
+            this.btnStartPause = new System.Windows.Forms.Button();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.rTxtBox = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRounds
+            // 
+            this.lblRounds.AutoSize = true;
+            this.lblRounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRounds.Location = new System.Drawing.Point(544, 28);
+            this.lblRounds.Name = "lblRounds";
+            this.lblRounds.Size = new System.Drawing.Size(87, 24);
+            this.lblRounds.TabIndex = 1;
+            this.lblRounds.Text = "Round: 0";
+            this.lblRounds.Click += new System.EventHandler(this.LblRounds_Click);
+            // 
+            // btnStartPause
+            // 
+            this.btnStartPause.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStartPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartPause.Location = new System.Drawing.Point(503, 55);
+            this.btnStartPause.Name = "btnStartPause";
+            this.btnStartPause.Size = new System.Drawing.Size(172, 56);
+            this.btnStartPause.TabIndex = 2;
+            this.btnStartPause.Text = "Start";
+            this.btnStartPause.UseVisualStyleBackColor = false;
+            this.btnStartPause.Click += new System.EventHandler(this.LblRounds_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.BackColor = System.Drawing.SystemColors.Control;
+            this.lblArea.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(12, 28);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(488, 333);
+            this.lblArea.TabIndex = 4;
+            // 
+            // rTxtBox
+            // 
+            this.rTxtBox.Location = new System.Drawing.Point(506, 164);
+            this.rTxtBox.Name = "rTxtBox";
+            this.rTxtBox.Size = new System.Drawing.Size(169, 183);
+            this.rTxtBox.TabIndex = 6;
+            this.rTxtBox.Text = "";
+            this.rTxtBox.TextChanged += new System.EventHandler(this.RTxtBox_TextChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(506, 117);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(83, 42);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.Location = new System.Drawing.Point(592, 117);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(83, 42);
+            this.btnRead.TabIndex = 8;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.BtnRead_Click);
+            // 
+            // frmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 382);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.rTxtBox);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.btnStartPause);
+            this.Controls.Add(this.lblRounds);
+            this.Name = "frmGame";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblRounds;
+        private System.Windows.Forms.Button btnStartPause;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.RichTextBox rTxtBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
